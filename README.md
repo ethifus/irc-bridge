e03076f6c7efce755dbb5ebc884a5736ede82b3d
Bartosz <ethifus@gmail.com>
2014-10-27 20:58:27 +0100

    Improvements + docs.

--- README.md
+++ README.md
@@ -5,11 +5,24 @@ IRC Bridge is a simple program that connects to two or more IRC channels by
 resending all messages captured on all channels to each of connected channels
 (without the one that received original message).
 
-Run it with:
+IRC Bridge depends on [go-ircevent](http://github.com/thoj/go-ircevent) library
+so you have to download it by:
+
+    $ go get github.com/thoj/go-ircevent
+
+You can run IRC Bridge with:
 
     $ irc_bridge config.json
 
 Configuration
 -------------
+
+ * *nicks* -- List of nicknames to try (if first is used, try second, etc).
+ * *username* -- Bot username.
+ * *servers* -- List of servers configurations. Each entry contains *name*,
+   *address* and *channel*.
+ * *template* -- Template for messages which is printed on each channel if
+   message from user is captured (it uses templating system from golang package
+   [text/tempate](http://golang.org/pkg/text/template/)).
 
 See [example.json](example.json).
--- irc_bridge.go
+++ irc_bridge.go
@@ -12,28 +12,30 @@ import (
 
 type ServerConfig struct {
 	Name    string
-	Adress  string
+	Address string
 	Channel string
 }
 
 type Configuration struct {
 	Nicks    []string
+	Username string
 	Servers  []ServerConfig
 	Template string
-}
-
-type Message struct {
-	Sender string
-	Nick   string
-	Body   string
 }
 
 type ServerConfigAll struct {
 	Server   ServerConfig
 	Nicks    []string
+	Username string
 	Sink     chan Message
 	Reciver  chan Message
 	Template *template.Template
+}
+
+type Message struct {
+	Sender string // Server's name, same as in ServerConfig.Name
+	Nick   string
+	Body   string
 }
 
 var logger = log.New(os.Stdout, "irc_bridge:", log.LstdFlags)
@@ -56,60 +58,74 @@ func loadConfig(configPath string) *Configuration {
 	return &config
 }
 
-// Connect to single server and join desired channel.
-func makeConnection(config ServerConfigAll) {
+// Setup all callbacks for given connection.
+func setupCallbacks(conn *irc.Connection, config ServerConfigAll) {
 	serverName := config.Server.Name
-	logger.Printf("[%s] (%s)\n", serverName, config.Server.Adress)
-	nickIndex := 0
-	conn := irc.IRC(config.Nicks[nickIndex], "Pan_Bot")
-	//conn.VerboseCallbackHandler = true
-	//conn.Debug = true
-
-	err := conn.Connect(config.Server.Adress)
-	if err != nil {
-		logger.Println(err.Error())
-		logger.Fatal("Can't connect to server.")
-	}
 
 	// join desired irc channel on connection success
 	conn.AddCallback("001", func(e *irc.Event) {
-		logger.Printf("[%s] join -> %s\n", serverName, config.Server.Channel)
+		logger.Printf("[%s] // join -> %s\n", serverName, config.Server.Channel)
 		conn.Join(config.Server.Channel)
 	})
 
 	// select another nickserverName if nick is already in use
+	nickIndex := 0
 	conn.AddCallback("433", func(e *irc.Event) {
 		nickIndex++
-		logger.Printf("[%s] 433 trying change nick to %s\n", serverName,
+		logger.Printf("[%s] // 433 trying change nick to %s\n", serverName,
 			config.Nicks[nickIndex])
 		conn.Nick(config.Nicks[nickIndex])
 	})
 
 	// recive message on irc channel and send it to config.Sink
 	conn.AddCallback("PRIVMSG", func(e *irc.Event) {
-		logger.Printf("[%s] <%s>: %s", serverName, e.Nick, e.Message())
-		config.Sink <- Message{
+		message := Message{
 			Sender: serverName,
 			Nick:   e.Nick,
 			Body:   e.Message(),
 		}
+		logger.Println(formatMessage(config.Template, message))
+		config.Sink <- message
 	})
+}
+
+// Convert given message to string usign given template.
+func formatMessage(template *template.Template, message Message) string {
+	var buffer bytes.Buffer
+
+	err := template.Execute(&buffer, message)
+	if err != nil {
+		logger.Fatalf("Invalid template: %s\n", err)
+	}
+
+	return buffer.String()
+}
+
+// Connect to single server and join desired channel.
+func makeConnection(config ServerConfigAll) {
+	logger.Printf("[%s] (%s/%s)\n", config.Server.Name, config.Server.Address,
+		config.Server.Channel)
+
+	conn := irc.IRC(config.Nicks[0], config.Username)
+	//conn.VerboseCallbackHandler = true
+	//conn.Debug = true
+
+	err := conn.Connect(config.Server.Address)
+	if err != nil {
+		logger.Println(err.Error())
+		logger.Fatal("Can't connect to server.")
+	}
+
+	setupCallbacks(conn, config)
 
 	go func() {
 		// wait for messsages on config.Reciver and write to irc channel only
 		// those that are not recived on this server/channel
 		for {
 			message := <-config.Reciver
-			logger.Printf("[%s] recived message: %s\n", serverName, message)
-			if message.Sender != serverName {
-				var buffer bytes.Buffer
-
-				err := config.Template.Execute(&buffer, message)
-				if err != nil {
-					logger.Fatalf("Invalid template: %s\n", err)
-				}
-
-				conn.Privmsg(config.Server.Channel, buffer.String())
+			if message.Sender != config.Server.Name {
+				text := formatMessage(config.Template, message)
+				conn.Privmsg(config.Server.Channel, text)
 			}
 		}
 	}()
@@ -130,6 +146,7 @@ func makeConnections(config *Configuration, sink chan Message) []chan Message {
 		serverConfig := ServerConfigAll{
 			Server:   server,
 			Nicks:    config.Nicks,
+			Username: config.Username,
 			Sink:     sink,
 			Reciver:  reciver,
 			Template: tmpl,
@@ -138,6 +155,16 @@ func makeConnections(config *Configuration, sink chan Message) []chan Message {
 		makeConnection(serverConfig)
 	}
 	return recivers
+}
+
+// Write all messages recived from sink to all recivers.
+func loop(sink chan Message, recivers []chan Message) {
+	for {
+		message := <-sink
+		for _, reciver := range recivers {
+			reciver <- message
+		}
+	}
 }
 
 func main() {
@@ -149,14 +176,5 @@ func main() {
 	sink := make(chan Message)
 	recivers := makeConnections(config, sink)
 
-	// write all messages recived from sink to all recivers
-	for {
-		message := <-sink
-		logger.Printf("recived message: [%s] <%s>: %s\n",
-			message.Sender, message.Nick, message.Body)
-		for i, reciver := range recivers {
-			logger.Printf("sending message to reciver nr %d\n", i)
-			reciver <- message
-		}
-	}
+	loop(sink, recivers)
 }
